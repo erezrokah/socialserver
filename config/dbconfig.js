166a71781c66aec846975f7149322f003708d2ca
Usman Abdur Rehman <saidskit@outlook.com>
2019-10-23 14:21:32 +0500

    Allah karey chal jaye 2

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,11 +1,19 @@
 var mysql = require('mysql');
 
-var db = mysql.createConnection({
+/*var db = mysql.createConnection({
   host     : 'localhost',
   user     : 'usmanrusso',
   password : 'usmanrusso',
   database : 'db'
+});*/
+
+var db = mysql.createConnection({
+  host     : 'remotemysql.com',
+  user     : '8oAOtD8KDy',
+  password : 'aWoEs2gKpx',
+  database : '8oAOtD8KDy'
 });
+
  
 db.connect(function(err){
 	if(err) throw err
